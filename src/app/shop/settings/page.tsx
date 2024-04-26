513d071eb148228d0046167f186f563ec5e1f136
RohitAmbati1 <ramenaimbot@gmail.com>
2024-04-25 19:08:10 -0500

    started changes on landing page

---
+++ src/app/shop/settings/page.tsx
@@ -0,0 +1,9 @@
+export default function Shop(){
+    return (
+        <main className="min-h-screen flex-col items-center justify-between p-24">
+            <h1>
+                Hello
+            </h1>
+        </main>
+    )
+}
