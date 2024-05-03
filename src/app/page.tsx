4824cb3237e8f66818ff14e71ed91326504afb99
RohitAmbati1 <ramenaimbot@gmail.com>
2024-05-02 19:06:54 -0500

    did button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const font = Abril_Fatface({weight: "400", subsets: ["latin"]})
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="pt-48 z-10 mb-5 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex sm:w-32">
         <Image 
             src="/Walmart_Spark.svg.png"
             alt="logo"
@@ -14,7 +14,12 @@ export default function Home() {
             height={270}
             priority
         />
-        <h1 className={`${font.className} text-9xl`}>WALLY WORLD!!!</h1>
+        <h1 className={`${font.className} text-9xl sm:text-8xl`}>WALLY WORLD!!!</h1>
+      </div>
+      <div className="lg:flex item-center justify-between mb-48">
+        <button className="bg-sky-500 hover:bg-sky-700 p-5 rounded-md">
+            Start Shopping
+        </button>
       </div>
     </main>
   );
