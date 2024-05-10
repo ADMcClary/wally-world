7a5be017cd4595f78f7a16ba5c843230f656c8bd
RohitAmbati1 <ramenaimbot@gmail.com>
2024-05-09 19:05:40 -0500

    navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex flex-row space-evenly w-full pl-10 bg-grey-600'>
+        <a href="#" className='flex w-full'>
+        type here:
+        </a>
+        <a href="#" className='flex w-full'>
+            food
+        </a >
+        <a href="#" className='flex w-full'>
+            clothes
+        </a>
+        <a href="#" className='flex w-full'>
+            toys
+        </a>
+        <a href="#" className='flex w-full'>
+            electronics
+        </a>
+
+    </div>
+  )
+}
+
+export default Navbar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
 import Image from "next/image";
 import { Abril_Fatface } from "next/font/google";
+import Navbar from "./components/Navbar";
 
 const font = Abril_Fatface({weight: "400", subsets: ["latin"]})
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="pt-48 z-10 mb-5 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex sm:w-32">
-        <Image 
-            src="/Walmart_Spark.svg.png"
-            alt="logo"
-            width={270}
-            height={270}
-            priority
-        />
-        <h1 className={`${font.className} text-9xl sm:text-8xl`}>WALLY WORLD!!!</h1>
+    <div>
+      <Navbar />
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="pt-48 z-10 mb-5 max-w-5xl lg:w-full items-center justify-between font-mono text-sm lg:flex sm:w-32 lg:pl-28">
+          <Image 
+              src="/Walmart_Spark.svg.png"
+              alt="logo"
+              width={270}
+              height={270}
+              priority
+          />
+          <h1 className={`${font.className} text-9xl sm:text-8xl`}>WALLY WORLD!!!</h1>
+        </div>
+        <div className="lg:flex item-center justify-between mb-48">
+          <button className="bg-sky-500 hover:bg-sky-700 p-5 rounded-md">
+              Start Shopping
+          </button>
+        </div>
       </div>
-      <div className="lg:flex item-center justify-between mb-48">
-        <button className="bg-sky-500 hover:bg-sky-700 p-5 rounded-md">
-            Start Shopping
-        </button>
-      </div>
-    </main>
+    </div>
   );
 }
