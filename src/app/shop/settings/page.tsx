f071ae7387a918472dfaa6f2695836b811e8f41b
FinleyA08 <fin.austin08@gmail.com>
2024-05-23 19:06:37 -0500

    Setup node again

--- src/app/shop/settings/page.tsx
+++ src/app/shop/settings/page.tsx
@@ -1,9 +1,7 @@
-export default function Shop(){
-    return (
-        <main className="min-h-screen flex-col items-center justify-between p-24">
-            <h1>
-                Hello
-            </h1>
-        </main>
-    )
+export default function Shop() {
+  return (
+    <main className="min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-red">Hello</h1>
+    </main>
+  );
 }
